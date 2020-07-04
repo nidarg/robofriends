d67191403b38966bccaff9c63fa79cb31b7e531f
nidarg <gradin_g@yahoo.com>
2020-07-05 02:16:49 +0300

    Updates

---
+++ precache-manifest.31db6974fad2b766436692844a9d0de4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cec55b338c12c065b755750af3476b3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d97c5ad7158eca63fc8",
+    "url": "/robofriends/static/css/main.80c986cd.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d97c5ad7158eca63fc8",
+    "url": "/robofriends/static/js/main.1568ede1.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
